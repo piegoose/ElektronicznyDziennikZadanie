bd65ba45c48504d9ea645fb1b9dbcdcde0f7e403
piegoose <jedrzej.sikora@puszkin.eu>
2025-01-23 17:11:54 +0100

    switch2

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,6 @@ public class Main {
         List<String> studentList = new ArrayList<>();
 
 
-
-
         System.out.println("Ilu jest studentow? ");
         int studentCount = scanner.nextInt();
         scanner.skip("\n");
@@ -20,65 +18,49 @@ public class Main {
             String studentPD = scanner.nextLine();
             studentList.add(studentPD);
         }
-            Collections.sort(studentList);
+        Collections.sort(studentList);
         for (int i = 0; i < studentList.size(); i++) {
             System.out.println((i + 1) + ": " + studentList.get(i));
 
         }
 
-     /// Modyfikacje Listy studentow:
+        /// Modyfikacje Listy studentow:
+
+
         System.out.println("--------Menu------------");
         System.out.println("1- Lista Studentow");
-        System.out.println("2- Usun Studenta");
+        System.out.println("2- Dodaj Studenta");
+        System.out.println("3- Usun Studenta");
+        System.out.println("4- Podzial na klasy");
 
         String input = scanner.next();
-        switch (input){
-            case "1"-> System.out.println(studentList);
-            // 2 - Dodaj osobe
+        switch (input) {
 
+            // 1 - Lista
+            case "1" -> System.out.println(studentList);
+            // 2 - Dodaj osobe
+            case "2" -> {
+                System.out.println("Podaj imie i nazwisko studenta: ");
+                String studentPD = scanner.nextLine();
+                studentList.add(studentPD);
+            }
             // 3 - Usun osobe
-            case "2"-> {
-                System.out.println("Podaj Numer indeksu studenta ktory ma byc usuniety: ");
-                int number = scanner.nextInt();
-                studentList.remove(number+1);
+            case "3" -> {
+                System.out.println("Podaj studenta ktory ma byc usuniety: ");
+                String personThrow = scanner.next();
+                studentList.remove(personThrow);
                 System.out.println(studentList);
 
             }
+
+
             // 9 - Zakoncz program
             // 10 - podzial na klasy ( mapy)
 
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
+
+}
