818adb55ef31026e83929f0ebb827f3999897617
piegoose <jedrzej.sikora@puszkin.eu>
2025-02-09 22:55:13 +0100

    09.02 Skonczone funkcjonalnosci dodawanaia usuwania Studenta i nauczyciela, Tworzenie klas in progress..

---
+++ src/MenuLauncher.java
@@ -0,0 +1,17 @@
+public class Menu {
+
+    public void menuLaunch() {
+
+        System.out.println("--------Menu------------");
+        System.out.println("1- Lista Studentow");
+        System.out.println("2- Dodaj Studenta");
+        System.out.println("3- Usun Studenta");
+        System.out.println("4- Podzial na klasy");
+        System.out.println("6 - Wyjdz z Dziennika");
+
+    }
+
+
+
+
+}
