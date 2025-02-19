e687e37696cf40b9a4f92244e0c328f914296dba
piegoose <jedrzej.sikora@puszkin.eu>
2025-02-19 16:08:01 +0100

    12.02 problem po zmianach staticow

--- src/Student.java
+++ src/Student.java
@@ -1,32 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.SequencedCollection;
 
 public class Student {
 
     // pola:
+    private String name;
+    private int id;
+    private double grade;
+    private List<Student> studentsList = new ArrayList<>();
 
 
-    public String name;
-    public int id;
-    public double grade;
-    public static List<Student> studentsList = new ArrayList<>();
-
-    //konstruktory:
-
+    //konstruktor
     public Student(String name, double grade) {
         this.name = name;
         this.id = id;
         this.grade = grade;
+        this.studentsList = studentsList;
+    }
+
+    public Student() {
+
     }
 
     // 3 - gettery i settery (+metody)
-
     public String getName() {
         return name;
     }
-
-
+    public List<Student> getStudentsList(){
+        return studentsList;
+    }
     public void displayStudentInfo(Student student) {
         System.out.println("Imie studenta: " + name);
         System.out.println("Indeks studenta: " + id);
@@ -35,18 +39,17 @@ public class Student {
 
     }
 
-    public static void studentAddToList(Scanner scanner) {
+    public  void studentAddToList(Scanner scanner) {
         System.out.println("Podaj imie i nazwisko studenta:");
         String name = scanner.next();
         scanner.nextLine();
-        //System.out.println("Podaj numer indeksu ");
         System.out.println("Podaj ocene koncowa studenta: ");
         double grade = scanner.nextDouble();
         studentsList.add(new Student(name, grade));
 
     }
 
-    public static void studentRemoveFromList(Scanner scanner) {
+    public  void studentRemoveFromList(Scanner scanner) {
 
         System.out.print("Nazwa studenta: ");
         String name = scanner.next();
@@ -56,6 +59,7 @@ public class Student {
 
     @Override
     public String toString() {
+
         return name + grade;
     }
 
